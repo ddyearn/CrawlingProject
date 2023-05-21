1f0de37e1d1790d1dd176a2b65d911fd2b77da82
Yoo SeungYeon <115228974+ddyearn@users.noreply.github.com>
2023-05-22 05:09:18 +0900

    Update main.py

--- CrawlEcommerce/main.py
+++ CrawlEcommerce/main.py
@@ -1,7 +1,7 @@
 import os
 import sys
 from eleventh.main import Eleventh
-# from naver.main import Naver
+from naver.main import Naver
 
 site_selection = 0 #0:coupang. 1:gmarket. 2:11st. 3:naver
 operation_mode = 0 #0:search. 1:best. 2:event. 3: category. 4:review
@@ -25,66 +25,68 @@ if len(sys.argv) >= 5:  #optional parameter, default value=0
 if len(sys.argv) >= 6:  #optional parameter, default value=0
     os_mode = int(sys.argv[5])
 
-# if site_selection == 0:
-#     #coupang site
-#     ### url 전체를 입력받는 대신에, 검색 키워드만을 입력 받아서 url을 자동생성하는 경우 코드 변경
-#     url = sys.argv[3]
-#     prefix = "http"
+if site_selection == 0:
+    #coupang site
+    ### url 전체를 입력받는 대신에, 검색 키워드만을 입력 받아서 url을 자동생성하는 경우 코드 변경
+    url = sys.argv[3]
+    prefix = "http"
 
-#     if not url.startswith(prefix):
-#         search_keyword = url
-#         url = url.replace(search_keyword, f"https://www.coupang.com/np/search?component=&q={search_keyword}&channel=user")
+    if not url.startswith(prefix):
+        search_keyword = url
+        url = url.replace(search_keyword, f"https://www.coupang.com/np/search?component=&q={search_keyword}&channel=user")
 
-#     coupang = Coupang(url, login_mode, os_mode)
+    coupang = Coupang(url, login_mode, os_mode)
 
-#     if operation_mode == 0:
-#         #search operation
-#         coupang.search()
-#         print("1")
-#     elif operation_mode == 1:
-#         #best operation
-#         coupang.best()
-#     elif operation_mode == 2:
-#         #event operation
-#         coupang.event()
-#     elif operation_mode == 3:
-#         #category operation
-#         coupang.category()
-#     elif operation_mode == 4:
-#         #review operation
-#         coupang.review()
-#     else:
-#         print("Operation Selection Error")
-# elif site_selection == 1:
-#     #gmarket site
-#     ### url 전체를 입력받는 대신에, 검색 키워드만을 입력 받아서 url을 자동생성하는 경우 코드 변경
-#     url = sys.argv[3]
-#     prefix = "http"
+    if operation_mode == 0:
+        #search operation
+        coupang.search()
+        print("1")
+    elif operation_mode == 1:
+        #best operation
+        coupang.best()
+    elif operation_mode == 2:
+        #event operation
+        coupang.event()
+    elif operation_mode == 3:
+        #category operation
+        coupang.category()
+    elif operation_mode == 4:
+        #review operation
+        coupang.review()
+    else:
+        print("Operation Selection Error")
 
-#     if not url.startswith(prefix):
-#         search_keyword = url
-#         url = url.replace(search_keyword, f"http://browse.gmarket.co.kr/search?keyword={search_keyword}")
+elif site_selection == 1:
+    #gmarket site
+    ### url 전체를 입력받는 대신에, 검색 키워드만을 입력 받아서 url을 자동생성하는 경우 코드 변경
+    url = sys.argv[3]
+    prefix = "http"
 
-#     gmarket = Gmarket(url, login_mode, os_mode)
+    if not url.startswith(prefix):
+        search_keyword = url
+        url = url.replace(search_keyword, f"http://browse.gmarket.co.kr/search?keyword={search_keyword}")
 
-#     if operation_mode == 0:
-#         #search operation
-#         gmarket.search()
-#     elif operation_mode == 1:
-#         #best operation
-#         gmarket.best()
-#     elif operation_mode == 2:
-#         #event operation
-#         gmarket.event()
-#     elif operation_mode == 3:
-#         #category operation
-#         gmarket.category()
-#     elif operation_mode == 4:
-#         #review operation
-#         gmarket.review()
-#     else:
-#         print("Operation Selection Error")
-if site_selection == 2:
+    gmarket = Gmarket(url, login_mode, os_mode)
+
+    if operation_mode == 0:
+        #search operation
+        gmarket.search()
+    elif operation_mode == 1:
+        #best operation
+        gmarket.best()
+    elif operation_mode == 2:
+        #event operation
+        gmarket.event()
+    elif operation_mode == 3:
+        #category operation
+        gmarket.category()
+    elif operation_mode == 4:
+        #review operation
+        gmarket.review()
+    else:
+        print("Operation Selection Error")
+
+elif site_selection == 2:
     #11st site
     ### url 전체를 입력받는 대신에, 검색 키워드만을 입력 받아서 url을 자동생성하는 경우 코드 변경
     url = sys.argv[3]
@@ -111,6 +113,36 @@ if site_selection == 2:
     elif operation_mode == 4:
         #review operation
         eleventh.review()
+    else:
+        print("Operation Selection Error")
+
+elif site_selection == 3:
+    #Naver site
+    ### url 전체를 입력받는 대신에, 검색 키워드만을 입력 받아서 url을 자동생성하는 경우 코드 변경
+    url = sys.argv[3]
+    prefix = "http"
+
+    if not url.startswith(prefix):
+        search_keyword = url
+        url = url.replace(search_keyword, f"https://search.shopping.naver.com/search/all?query={search_keyword}")
+
+    naver = Naver(url, login_mode, os_mode)
+
+    if operation_mode == 0:
+        #search operation
+        naver.search()
+    elif operation_mode == 1:
+        #best operation
+        naver.best()
+    elif operation_mode == 2:
+        #event operation
+        naver.event()
+    elif operation_mode == 3:
+        #category operation
+        naver.category()
+    elif operation_mode == 4:
+        #review operation
+        naver.review()
     else:
         print("Operation Selection Error")
 else:
